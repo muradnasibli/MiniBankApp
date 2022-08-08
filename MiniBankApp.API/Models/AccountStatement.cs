ab17af494bc464baafb5c00a0d291cfa27501a48
Murad Nasibli <muradnasibli@Murads-MacBook-Pro.local>
2022-08-09 01:25:14 +0400

    AccountStatement model created

--- MiniBankApp.API/Models/AccountStatement.cs
+++ MiniBankApp.API/Models/AccountStatement.cs
@@ -2,5 +2,9 @@ namespace MiniBankApp.API.Models;
 
 public class AccountStatement
 {
-    
+    public string TransactionDate { get; set; }
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+    public decimal Remain { get; set; }
+    public bool Income { get; set; }
 }
