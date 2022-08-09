0f0539aa044144c0f0c3a9880565467d743853f0
Murad Nasibli <muradnasibli@Murads-MacBook-Pro.local>
2022-08-09 10:18:01 +0400

    Added lifecycles for IConvert, IFileService, IAccountService

--- MiniBankApp.API/Program.cs
+++ MiniBankApp.API/Program.cs
@@ -1,3 +1,9 @@
+using MiniBankApp.API.Helpers;
+using MiniBankApp.API.Helpers.Base;
+using MiniBankApp.API.Models;
+using MiniBankApp.API.Services;
+using MiniBankApp.API.Services.Base;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IConvert<AccountInformation>, JsonConverterService<AccountInformation>>();
+builder.Services.AddSingleton<IFileService, FileService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
 
 var app = builder.Build();
 
