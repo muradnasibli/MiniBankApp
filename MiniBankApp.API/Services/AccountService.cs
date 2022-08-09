939866609f00b2104b97c1beebf517b3a8a19a10
Murad Nasibli <muradnasibli@Murads-MacBook-Pro.local>
2022-08-09 10:11:23 +0400

    GetAccountInformation created

--- MiniBankApp.API/Services/AccountService.cs
+++ MiniBankApp.API/Services/AccountService.cs
@@ -1,6 +1,33 @@
+using MiniBankApp.API.Helpers.Base;
+using MiniBankApp.API.Models;
+using MiniBankApp.API.Services.Base;
+
 namespace MiniBankApp.API.Services;
 
-public class AccountService
+public class AccountService: IAccountService
 {
+    private readonly IFileService _fileService;
+    private readonly IConvert<AccountInformation> _convert;
     
+    public AccountService(IFileService fileService, IConvert<AccountInformation> convert)
+    {
+        _fileService = fileService;
+        _convert = convert;
+    }
+
+    public AccountInformation GetAccountInformation()
+    {
+        var data = _fileService.ReadFileFromAppSettings("jsonData");
+        return _convert.Deserialize(data);
+    }
+
+    public List<AccountStatement> GetMonthlyIncome()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<AccountStatement> GetMonthlyOutcome()
+    {
+        throw new NotImplementedException();
+    }
 }
