dda59deb44a76a995c016aa93578a75a106a21de
Murad Nasibli <muradnasibli@Murads-MacBook-Pro.local>
2022-08-09 10:15:37 +0400

    Deserialize, Serialize options created

--- MiniBankApp.API/Helpers/JsonConverterService.cs
+++ MiniBankApp.API/Helpers/JsonConverterService.cs
@@ -1,6 +1,24 @@
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+using MiniBankApp.API.Helpers.Base;
+
 namespace MiniBankApp.API.Helpers;
 
-public class JsonConverterService
+public class JsonConverterService<T> : IConvert<T>
 {
-    
+    public string Serialize(T entity)
+    {
+        return JsonSerializer.Serialize(entity);
+    }
+
+    public T Deserialize(string data)
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+        
+        return JsonSerializer.Deserialize<T>(data, options);
+    }
 }
