dcd29737041c6159db6850f22e9b4c05a8528479
muradnasibli <muradin@code.edu.az>
2022-08-11 02:33:19 +0400

    Remain property removed from AccountStatement class

--- MiniBankApp.API/Models/AccountStatement.cs
+++ MiniBankApp.API/Models/AccountStatement.cs
@@ -5,6 +5,5 @@ public class AccountStatement
     public string TransactionDate { get; set; }
     public string Description { get; set; }
     public decimal Amount { get; set; }
-    public decimal Remain { get; set; }
     public bool Income { get; set; }
 }
